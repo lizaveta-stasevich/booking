4feba50da6634b9cf13c8b60dcafc95c19eee1fd
Liza <lizik.stasevich@gmail.com>
2019-10-15 12:16:44 +0300

    !

--- src/core/urls.py
+++ src/core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("main/", include("main.urls")),
+    path("", include("main.urls")),
+    path("news/", include("news.urls")),
     path('admin/', admin.site.urls),
 ]
