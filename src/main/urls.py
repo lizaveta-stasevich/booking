c09cdee871f958abc7f22dae25e49039b68f2121
Liza <lizik.stasevich@gmail.com>
2019-10-22 19:59:27 +0300

    styles added

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-from . import views
-
-urlpatterns = [
-    path("", views.main, name='main'),
-]
+urlpatterns = [path("", TemplateView.as_view(template_name="main/index.html"), name='main', )]
