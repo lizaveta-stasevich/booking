12088a1ededd62033a3c8bc32852e536f2615b40
Liza <lizik.stasevich@gmail.com>
2019-10-23 19:30:21 +0300

    static

--- src/core/settings.py
+++ src/core/settings.py
@@ -83,11 +83,11 @@ if _settings.ENV_FOR_DYNACONF == "heroku":
     db_url = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       }
-    # "default": dj_database_url.parse(db_url, conn_max_age=600),
+    #'default': {
+     # 'ENGINE': 'django.db.backends.sqlite3',
+      #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       #}
+    "default": dj_database_url.parse(db_url, conn_max_age=600),
 }
 
 # Password validation
